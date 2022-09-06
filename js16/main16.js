55205bc82bd9b5437ca63296e8232bc4f77c8695
Nikita <lun9lol@gmail.com>
2022-09-06 20:36:01 +0300

    правки dz16

--- js16/main16.js
+++ js16/main16.js
@@ -234,22 +234,22 @@ let testPerformance = (iterations, func) => {
 };
 
 // данная функция необходима для корректного тестирования кода
-// function test1() {
-//   let str = myLongStr;
-//   while (str.indexOf('o') !== -1) str = str.replace('o', '');
-//   while (str.indexOf('a') !== -1) str = str.replace('a', '');
-//   while (str.indexOf('e') !== -1) str = str.replace('e', '');
-//   while (str.indexOf('u') !== -1) str = str.replace('u', '');
-//   while (str.indexOf('i') !== -1) str = str.replace('i', '');
-// }
+function test1() {
+  let str = myLongStr;
+  while (str.indexOf("o") !== -1) str = str.replace("o", "");
+  while (str.indexOf("a") !== -1) str = str.replace("a", "");
+  while (str.indexOf("e") !== -1) str = str.replace("e", "");
+  while (str.indexOf("u") !== -1) str = str.replace("u", "");
+  while (str.indexOf("i") !== -1) str = str.replace("i", "");
+}
 
 // данная функция необходима для корректного тестирования кода
-// function test2() {
-//   const reg = new RegExp('[oaeui]', 'gui');
+function test2() {
+  const reg = new RegExp("[oaeui]", "gui");
 
-//   myLongStr.replace(reg, '');
-// }
+  myLongStr.replace(reg, "");
+}
 
-// console.log(testPerformance(100, test1)); // time
-// console.log(testPerformance(100, test2)); // time
-// console.log(testPerformance(100, 12345)); // 0
+console.log(testPerformance(100, test1)); // time
+console.log(testPerformance(100, test2)); // time
+console.log(testPerformance(100, 12345)); // 0
